6ffb7a45ed0c09395fac655b0e7e1b7900e06337
Joe <joe_jing_91@163.com>
2025-03-31 10:29:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 import sys
 import hashlib
-import asyncio
-import aiofiles
 import json
 
 """
@@ -19,24 +17,37 @@ def search_files(dir_path):
     result = []
     file_list = os.listdir(dir_path)
     for file_name in file_list:
-        complete_file_name = os.path.join(dir_path, file_name)
-        if os.path.isdir(complete_file_name):
-            result.extend(search_files(complete_file_name))
-        if os.path.isfile(complete_file_name):
-            result.append(complete_file_name)
-        print("search: " + complete_file_name)
+        try:
+            complete_file_name = os.path.join(dir_path, file_name)
+            print("search: " + complete_file_name)
+            if os.path.isdir(complete_file_name):
+                result.extend(search_files(complete_file_name))
+            if os.path.isfile(complete_file_name):
+                result.append(complete_file_name)
+        except:
+            print("find the file cannot be decoded, skip") 
+            continue
     return result
 
 
-async def get_repeat(file_list) -> dict:
+#block 500MB
+def calc_file_md5(f, block_size=(2 ** 20) * 500):
+    md5_hash = hashlib.md5()
+    while True:
+        data = f.read(block_size)
+        if not data:
+            break
+        md5_hash.update(data)
+    return md5_hash.hexdigest()
+
+
+def get_repeat(file_list) -> dict:
     database = {}
     for file in file_list:
-        async with aiofiles.open(file, "rb") as f:
+        with open(file, "rb") as f:
             file_size = os.stat(file).st_size
             print("read file[size:{:,d}]: {:s}".format(file_size, file))
-            data = await f.read()
-            data = ""
-            md5 = hashlib.md5(data).hexdigest()
+            md5 = calc_file_md5(f)
             if md5 not in database:
                 database[md5] = []
                 database[md5].append(file)
@@ -47,13 +58,13 @@ async def get_repeat(file_list) -> dict:
     return database
 
 
-async def main(dir_name):
+def main(dir_name):
     if os.path.exists(OUTFILE):
         os.remove(OUTFILE)
     file_list = search_files(dir_name)
-    database = await get_repeat(file_list)
-    print(len(file_list))
-    print(len(database))
+    database = get_repeat(file_list)
+    print("list len: " + len(file_list))
+    print("database len: " + len(database))
 
 
 def remove_repeat(database_file):
@@ -68,14 +79,31 @@ def remove_repeat(database_file):
             else:
                 break
 
+def print_result(database_file):
+    with open(database_file, "r") as f:
+        database = json.load(f)
+    for md5 in database:
+        count = len(database[md5])
+        print("md5[{:s}], find [{:d}] files".format(md5,count))
+        for file in database[md5]:
+            if(count) > 1:
+                print("duplicated file: " + file)
+#            else:
+#                print("file: " + file)
+            
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         if sys.argv[1] == "scan":
-            asyncio.run(main(sys.argv[2]))
+            main(sys.argv[2])
+        elif sys.argv[1] == "result":
+            print_result(sys.argv[2])
         elif sys.argv[1] == "remove":
             remove_repeat(sys.argv[2])
     else:
         print("scan and remove repeat files")
         print("scan: python main.py scan <dir_path>")
+        print("result: python main.py result <output.json>")
         print("remove: python main.py remove <output.json>")
+
